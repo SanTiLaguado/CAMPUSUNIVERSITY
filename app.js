df39ea826cd7856c342b518400573ef7e6acff47
santilaguado <santiagolaguado57@gmail.com>
2024-03-02 21:08:24 -0500

    v0.0.5

--- app.js
+++ app.js
@@ -1,79 +1,63 @@
-document.addEventListener('DOMContentLoaded',async ()=>{
-await cargarProgramas();
-cargarFormularioEstudiantes();
-await cargarEstudiantes();
-})
+document.addEventListener('DOMContentLoaded', async () => {
+    await handleNavigation();
+    await cargarProgramas();
+    cargarFormularioEstudiantes();
+    await cargarEstudiantes();
 
-
-document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('.navigation a');
-
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
+    links.forEach(link => {
+        link.addEventListener('click', event => {
             event.preventDefault();
-            // Llama a la función handleNavigation con el texto del enlace como argumento
             handleNavigation(link.textContent);
         });
     });
 });
 
-function handleNavigation(linkText) {
-    // Aquí puedes escribir el código que deseas ejecutar cuando se hace clic en un enlace
-    console.log('Hiciste clic en:', linkText);
-    // Dependiendo del texto del enlace, puedes realizar diferentes acciones
-    switch (linkText) {
-        case 'Inicio':
-            console.log("home")
-            break;
-    // Informes
-        case 'Informe Matriculas':
-            console.log("Informe Matriculas")
-            break;
-        case 'Inicio':
-            console.log("nforme Asignaturas")
-            break;
-        case 'Informe Matriculas':
-            console.log("Informe Asignaturas")
-            break;
-        case 'Generar Horarios':
-            console.log("Generar Horarios")
-            break;
-    // Datos Existentes
-        case 'Programas':
-            console.log("Programas")
-            break;
-        case 'Periodos':
-            console.log("Periodos")
-            break;
-        case 'Tarifas':
-            console.log("Tarifas")
-            break;
-        case 'Departamentos':
-            console.log("Departamentos")
-            break;
-        case 'Salones':
-            console.log("Salones")
-            break;
-    // Gestion
-         case 'Estudiantes':
-            console.log("Estudiantes")
-            break;
-        case 'Docentes':
-            console.log("Docentes")
-            break;
-        case 'Asignaturas':
-            console.log("Asignaturas")
-            break;
-    // Matriculas
-         case 'Nueva Matricula':
-            console.log("Nueva Matricula")
-            break;
-        case 'Lista de Matriculas':
-            console.log("Lista de Matriculas")
-            break;
+// Navegacion
 
-        default:
-            // Código de Home
-            break;
+const navigateToSection = (sectionId) => {
+    const divs = document.querySelectorAll('main > section');
+    
+    divs.forEach(div => {
+        if (div.id !== sectionId) {
+            div.style.display = 'none';
+        }
+    });
+
+    const sectionToShow = document.getElementById(sectionId);
+    if (sectionToShow) {
+        sectionToShow.style.display = 'flex';
     }
+}
+
+const sectionMap = {
+    'Inicio': 'Inicio',
+    // Informes
+    'Informes': 'inforMatriculas',
+    'Informe Matriculas': 'inforMatriculas',
+    'Informe Asignaturas': 'inforAsignatura',
+    'Generar Horarios': 'Horarios',
+    // Datos Existentes
+    'Datos Existentes': 'Programas',
+    'Programas': 'Programas',
+    'Periodos': 'Periodos',
+    'Tarifas': 'Tarifas',
+    'Departamentos': 'Departamentos',
+    'Salones': 'Salones',
+    // Gestion
+    'Gestion': 'Estudiantes',
+    'Estudiantes': 'Estudiantes',
+    'Docentes': 'Docentes',
+    'Asignaturas': 'Asignaturas',
+    // Matriculas
+    'Matriculas' : 'Nueva-Matricula',
+    'Nueva Matricula': 'Nueva-Matricula',
+    'Lista de Matriculas': 'Lista-Matriculas'
+};
+
+
+function handleNavigation(linkText) {
+    console.log('Hiciste clic en:', linkText);
+    const sectionId = sectionMap[linkText] || 'Inicio';
+    navigateToSection(sectionId);
 }
